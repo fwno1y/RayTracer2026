d69e75faba40965236446fc380961838b7f7d7db
fwno1y <20070119@sjtu.edu.cn>
2026-07-07 20:10:52 +0800

    fix code format again

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,10 @@ fn main() {
     let pixel_delta_u = viewport_u / image_width as f64;
     let pixel_delta_v = viewport_v / image_height as f64;
 
-    let viewport_upper_left =
-        camera_center - Vec3::new_vec3(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let viewport_upper_left = camera_center
+        - Vec3::new_vec3(0.0, 0.0, focal_length)
+        - viewport_u / 2.0
+        - viewport_v / 2.0;
     let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     let width = image_width as u32;
