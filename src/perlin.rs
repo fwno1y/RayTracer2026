151c4d4172a2b9a562a04395e682e89fa3e64119
fwno1y <20070119@sjtu.edu.cn>
2026-07-13 09:50:14 +0800

    finish book2-image10

--- src/perlin.rs
+++ src/perlin.rs
@@ -25,11 +25,25 @@ impl Perlin {
             perm_z,
         }
     }
+    #[allow(clippy::needless_range_loop)]
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
-        self.randfloat[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]]
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[self.perm_x[(i as usize + di) & 255]
+                        ^ self.perm_y[(j as usize + dj) & 255]
+                        ^ self.perm_z[(k as usize + dk) & 255]];
+                }
+            }
+        }
+        Self::trilinear_interp(c, u, v, w)
     }
     fn perlin_generate_perm() -> [usize; POINT_COUNT as usize] {
         let mut p = [0; POINT_COUNT as usize];
@@ -41,5 +55,20 @@ impl Perlin {
             p.swap(i as usize, target as usize);
         }
         p
+    }
+    #[allow(clippy::needless_range_loop)]
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
+                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
+                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
     }
 }
